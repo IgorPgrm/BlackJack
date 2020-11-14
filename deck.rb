cd7d75eaeeb8271e4d3e4a1b4a0cf88ddf824fc9
Igor_Shum <Shumeikoigor@yandex.ru>
2020-11-14 22:58:10 +0300

    Переименован атрибут

--- deck.rb
+++ deck.rb
@@ -1,17 +1,17 @@
 require_relative 'card'
 class Deck
-  attr_reader :count, :deck, :deck_count
+  attr_reader :count, :cards, :deck_count
 
   SUITS = ['♥', '♢', '♧', '♤'].freeze
   CARDS = [*(2..10), 'J', 'Q', 'K', 'A'].freeze
 
   def initialize(deck_count = 1)
     @deck_count = deck_count
-    @deck = []
+    @cards = []
     @deck_count.times do
       SUITS.each do |suit|
         CARDS.each do |card|
-          @deck << Card.new(suit, card)
+          @cards << Card.new(suit, card)
         end
       end
     end
