32a1ba2dd8141f344e60346e017d80a49287cf17
Igor_Shum <Shumeikoigor@yandex.ru>
2020-11-14 23:48:09 +0300

    +Методы добавления и отображения карты

--- player.rb
+++ player.rb
@@ -1,8 +1,21 @@
 class Player
-  attr_reader :name, :balance, :cards
+  attr_reader :name, :balance, :cards, :card_weight
 
   def initialize(name, balance)
     @name = name
     @balance = balance
+    @cards = []
+    @card_weight = 0
+  end
+
+  def add_card(card)
+    @cards << card
+    @card_weight += card.cost
+  end
+
+  def show_cards
+    @cards.each do |card|
+      puts "#{card.suit} #{card.name}"
+    end
   end
 end
