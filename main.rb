779d2315db9769ca9f6932f8585012bfd780b391
Igor_Shum <Shumeikoigor@yandex.ru>
2020-11-15 20:15:04 +0300

    +Методы, отображения статуса игры, меню выбора действий и взятие карт

--- main.rb
+++ main.rb
@@ -4,10 +4,24 @@ class Main
     puts 'Введите своё имя:'
     input = gets.chomp.to_s
     @main_game = Game.new(input, 100)
+    show_info
+    menu
   end
 
   def clear
     system('clear')
+  end
+
+  def show_info
+    @main_game.player.show_cards
+  end
+
+  def menu
+    @main_menu = ""
+  end
+
+  def hit
+    @main_game.player.add_card @main_game.deck
   end
 end
 
