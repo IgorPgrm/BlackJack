7b479f17c04fb1075e4098b95a7fa0d415337e8e
Igor_Shum <Shumeikoigor@yandex.ru>
2020-11-15 20:12:44 +0300

    +Методы создания новой колоды и перемешивания карт

--- deck.rb
+++ deck.rb
@@ -7,13 +7,26 @@ class Deck
 
   def initialize(deck_count = 1)
     @deck_count = deck_count
+    new_deck(@deck_count)
+  end
+
+  def new_deck(deck_count)
     @cards = []
-    @deck_count.times do
+    deck_count.times do
       SUITS.each do |suit|
         CARDS.each do |card|
           @cards << Card.new(suit, card)
         end
       end
     end
+    shuffle_cards
+  end
+
+  def shuffle_cards
+    @cards.shuffle!.shuffle!.shuffle!.shuffle!
+  end
+
+  def take_card
+    @cards&.shift
   end
 end
