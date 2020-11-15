0e169da55bb59403065b9dcd44f8fc441b4403d3
Igor_Shum <Shumeikoigor@yandex.ru>
2020-11-15 18:31:19 +0300

    Скрытая карта диллера

--- dealer.rb
+++ dealer.rb
@@ -2,4 +2,17 @@ class Dealer < Player
   def initialize
     super('Dealer', 100)
   end
+
+  def face_down
+    symb = '╳'
+    puts '╭╶╶╶╶╶╶╶╶╶╶╮'
+    9.times do
+      print '╵'
+      10.times do
+        print symb
+      end
+      puts '╵'
+    end
+    puts '╰╶╶╶╶╶╶╶╶╶╶╯'
+  end
 end
