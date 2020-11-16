95a4655273477d67ee7fe2d98a2e2c963870b36e
Igor_Shum <Shumeikoigor@yandex.ru>
2020-11-16 20:38:01 +0300

    Метод отрисовки карт диллера (1 открыта, все последующие закрыты)

--- dealer.rb
+++ dealer.rb
@@ -15,4 +15,36 @@ class Dealer < Player
     end
     puts '╰╶╶╶╶╶╶╶╶╶╶╯'
   end
+
+  def dealer_cards_show
+    show_cards_line = ''
+    top = ''
+    label = ''
+    line = ''
+    bottom = ''
+    symb = '╵╳╳╳╳╳╳╳╳╳╳╵'
+    puts @cards.length
+    @cards.each do |card|
+      top += '╭╶╶╶╶╶╶╶╶╶╶╮'
+      if card == @cards.first
+        suit_name = "#{card.suit} #{card.name}"
+        label += if card.name == 10
+                   "╵      #{suit_name}╵"
+                 else
+                   "╵       #{suit_name}╵"
+                 end
+        line += '╵          ╵'
+      else
+        label += symb
+        line += symb
+      end
+      bottom += '╰╶╶╶╶╶╶╶╶╶╶╯'
+    end
+    show_cards_line += "#{top}\n#{label}\n"
+    7.times do
+      show_cards_line += "#{line}\n"
+    end
+    show_cards_line += bottom
+    puts show_cards_line
+  end
 end
