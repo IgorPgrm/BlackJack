79e2192f68d11b6246f968542f48e45976a3cafd
Igor_Shum <Shumeikoigor@yandex.ru>
2020-11-18 16:49:58 +0300

    Добавлены Раздача карт, статус бар, аттрибуты

--- game.rb
+++ game.rb
@@ -3,26 +3,62 @@ require_relative 'player'
 require_relative 'dealer'
 
 class Game
-  attr_accessor :player, :money, :bank, :deck
+  attr_accessor :player, :dealer, :money, :bank, :deck, :bet
 
   def initialize(player_name, money = 100, deck_count = 1)
+    @na = 'N/a'.to_sym
     @money = money
     @deck = Deck.new(deck_count)
     @player = Player.new(player_name, @money)
     @dealer = Dealer.new
     @players = [@player, @dealer]
+    @status = 0
+    @bet = 0
+    @status = 0
     start_game
   end
 
+  def clear
+    system('clear')
+  end
+
+  def status_bar
+    print 'Баланс:['
+    print @money.zero? ? "\u001B[31m 0 \u001B[0m" : "\u001B[32m#{@money}\u001B[0m"
+    print '] ставка:['
+    print @bet.zero? ? "\u001B[31m#{@na}\u001B[0m" : "\u001B[32m#{@bet}\u001B[0m"
+    print '] Очки:['
+    if @player.points.instance_of? Array
+      print "\u001B[32m#{@player.points.max}\u001B[0m"
+    elsif @player.points.instance_of? Integer
+      @player.points.zero? ? "\u001B[31m 0 \u001B[0m" : "\u001B[32m#{@player.points}\u001B[0m"
+    end
+    print '] Статус:['
+    print @status.zero? ? "\u001B[31m#{@status}\u001B[0m" : "\u001B[32m#{@status}\u001B[0m"
+    print "]\n"
+  end
+
   def start_game
-    puts 'Выдаём карты...'
+    puts 'Раздача карт...'
+    sleep(0.4)
     2.times do
+      clear
       @players.each do |player|
-        puts "Player: #{player.name} +1"
         player.add_card @deck.cards.shift
+        player.show_cards if player.instance_of? Player
+        player.dealer_cards_show if player.instance_of? Dealer
+        sleep(0.4)
       end
     end
   end
 
+  def queue
+    loop do
+      break if over?
+    end
+  end
 
+  def over?
+    false
+  end
 end
