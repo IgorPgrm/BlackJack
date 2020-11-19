e5c110cc8ef739f3edb9360ce8b951ec757ad2d4
IgorShum <Shumeikoigor@yandex.ru>
2020-11-19 14:48:31 +0300

    Временная переменная

--- main.rb
+++ main.rb
@@ -2,15 +2,15 @@ require_relative 'game'
 class Main
   def initialize
     puts 'Введите своё имя:'
-    #input = gets.chomp.to_s
-    input = "Igor"
+    # input = gets.chomp.to_s
+    input = 'Igor'
     @main_game = Game.new(input, 100)
     show_info
     menu
-    puts "Очки: #{@main_game.player.points}"
   end
 
   def show_info
+    @main_game.clear
     @main_game.status_bar
     @main_game.dealer.dealer_cards_show
     @main_game.player.show_cards
