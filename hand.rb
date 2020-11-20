20e1c60e1b3b7d1b41bee81f04b10947fb25e5e7
IgorShum <Shumeikoigor@yandex.ru>
2020-11-20 23:58:13 +0300

    Метод отображения карт для руки

--- hand.rb
+++ hand.rb
@@ -7,4 +7,29 @@ class Hand
     @cards << card
   end
 
+  def cards_show
+    show_cards_line = ''
+    top = ''
+    label = ''
+    line = ''
+    bottom = ''
+    @cards.each do |card|
+      top += '╭╶╶╶╶╶╶╶╶╶╶╮'
+      suit_name = "#{card.suit} #{card.name}"
+      label += if card.name == 10
+                 "╵      #{suit_name}╵"
+               else
+                 "╵       #{suit_name}╵"
+               end
+      line += '╵          ╵'
+      bottom += '╰╶╶╶╶╶╶╶╶╶╶╯'
+    end
+    show_cards_line += "#{top}\n#{label}\n"
+    7.times do
+      show_cards_line += "#{line}\n"
+    end
+    show_cards_line += bottom
+    puts show_cards_line
+  end
+
 end
