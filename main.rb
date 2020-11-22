bd1e7bd65a06708b2c9a7e43e64c01aabb0fce03
IgorShum <Shumeikoigor@yandex.ru>
2020-11-22 20:26:11 +0300

    Навигационное меню, вынос вызовов методов в отдельные методы

--- main.rb
+++ main.rb
@@ -13,22 +13,45 @@ class Main
     puts <<NGM
     Игра окончена!
     1 - Начать новую игру?
-    2 -Выход
+    2 - Изменить ставку
+    0 -Выход
 NGM
     input = gets.chomp.to_i
     case input
+    when 0
+      exit
+    when 2
+      @main_game.set_bet
+      start_new_game
     when 1
-      @main_game.status = ''
-      @main_game.start_game
-      @main_game.clear
-      @main_game.show_cards
-      menu
+      start_new_game
     end
+  end
+
+  def start_new_game
+    @main_game.bet = @main_game.current_bet
+    @main_game.status = ''
+    @main_game.start_game
+    @main_game.clear
+    @main_game.show_cards
+    menu
   end
 
   def show_info
     @main_game.show_cards
     @main_game.over? ? new_game_menu : menu
+  end
+
+  def take_card_scr
+    @main_game.hit
+    @main_game.clear
+  end
+
+  def open_cards
+    winner = @main_game.winning
+    @main_game.give_bank(winner)
+    @main_game.show_cards(true)
+    new_game_menu
   end
 
   def menu
@@ -41,13 +64,15 @@ NGM
     input = gets.chomp.to_i
     case input
     when 1
-      @main_game.hit
-      @main_game.clear
+      take_card_scr
       show_info
     when 2
-      @main_game.show_cards(true)
-      # >check points
-      new_game_menu
+      open_cards
+    when 3
+      @main_game.player.balance -= @main_game.bet
+      @main_game.bet *= 2
+      take_card_scr
+      open_cards
     end
   end
 end
